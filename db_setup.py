faf083631e2fd0f266d4ae7d1b70cf5168f8187f
Bryan Brunetti <bryan.brunetti@gmail.com>
2020-04-23 00:34:51 -0400

    found a better word list to use

--- db_setup.py
+++ db_setup.py
@@ -2,23 +2,34 @@ from redis import Redis
 
 r = Redis()
 words = open("words_alpha.txt").readlines()
-doublet_list = dict()
+bigram_list = dict()
 
-print(f"Found {len(words)} words, clearing existing entries ...")
 r.delete("words")
 r.delete("letterpairs")
-count = 0
+print(f"Processing {len(words)} words ", end='', flush=True)
 
+count = 1
+checkpoint = int(len(words) / 10)
 
 for word in words:
-    word = word.rstrip()
+    if not checkpoint % count:
+        print(".", end='', flush=True)
+    count += 1
+    word = word.rstrip().lower()
     if len(word) >= 2:
         r.sadd("words", word)
-        for i,j in enumerate(word):
-            doublet = word[i:i+2]
-            if len(doublet) == 2:
-                doublet_list.setdefault(doublet, 0)
-                doublet_list[doublet] += 1
+        for i, j in enumerate(word):
+            bigram = word[i:i + 2]
+            if len(bigram) == 2:
+                bigram_list.setdefault(bigram, 0)
+                bigram_list[bigram] += 1
 
-for pair,freq in doublet_list.items():
+print(f"Processing {len(bigram_list)} bigrams ", end="", flush=True)
+count = 1
+checkpoint = int(len(bigram_list) / 10)
+
+for pair, freq in bigram_list.items():
+    if not checkpoint % count:
+        print(".", end='', flush=True)
+    count += 1
     r.zadd("letterpairs", {pair: freq})
